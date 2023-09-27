83f4495b3ac349e68f34dd7425577d07e32421c0
Carl Harris <hitbox@ryzen.com>
2023-09-27 08:00:29 -0400

    renamed and cleanup up oauth funcs

--- flask_okta/oauth.py
+++ flask_okta/oauth.py
@@ -1,23 +1,20 @@
 import base64
 import hashlib
 import secrets
-import string
 
-def generate_code_verifier(n=128):
-    # https://www.oauth.com/oauth2-servers/pkce/authorization-request/
-    # https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
-    alphabet = string.ascii_letters + string.punctuation
-    code_verifier = ''.join(secrets.choice(alphabet) for _ in range(n))
+# NOTE
+# generate_code_verifier and generate_state_token are generated as url safe in
+# Okta examples and there were problems not using url safe in development. If
+# we wait, and make their values url safe when the url is created, we get
+# different values in the session to compare to.
+# nbytes was made 64 because Okta examples were.
+
+def generate_code_verifier(nbytes=64):
+    code_verifier = secrets.token_urlsafe(nbytes)
     return code_verifier
 
-def generate_code_verifier(n=64):
-    # XXX
-    # overwriting name for now
-    code_verifier = secrets.token_urlsafe(n)
-    return code_verifier
-
-def generate_state_token(n=64):
-    state = secrets.token_urlsafe(n)
+def generate_state_token(nbytes=64):
+    state = secrets.token_urlsafe(nbytes)
     return state
 
 def get_code_challenge(code_verifier):
