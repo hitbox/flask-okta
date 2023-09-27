dcc280af022129bf5c68d2a7797058f649f9db98
Carl Harris <hitbox@ryzen.com>
2023-09-27 08:01:16 -0400

    fix typo

--- flask_okta/oauth.py
+++ flask_okta/oauth.py
@@ -21,7 +21,7 @@ def get_code_challenge(code_verifier):
     hash_ = hashlib.sha256(code_verifier.encode()).digest()
     code_challenge = base64.urlsafe_b64encode(hash_)
     # https://www.oauth.com/oauth2-servers/pkce/authorization-request/
-    # oath/okta required changes to base 64 encoding
+    # oauth/okta required changes to base 64 encoding
     # change + to -, / to _, and strip =
     # https://www.oauth.com/oauth2-servers/pkce/authorization-request/
     code_challenge = code_challenge.decode('ascii').rstrip('=')
